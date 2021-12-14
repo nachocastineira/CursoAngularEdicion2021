4d20a080bf327361ea61fa71ead87fe8e3797a6f
nachocastineira <nacho2.0804@gmail.com>
2021-12-14 15:36:08 -0300

    seccion 09-mapasApp

---
+++ 09-mapasApp/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from '../environments/environment.prod';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+     (mapboxgl as any).accessToken = environment.mapboxToken;
+  }
+  
+}
---
+++ 09-mapasApp/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoibmFjaG9jYXN0aW5laXJhIiwiYSI6ImNreDZlMmtubzFoem8ybnBtcHB0d3FsZWsifQ.dtsrPawvejmtlo8Js39IRg'
+};
